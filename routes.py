4d6830cf215e2b44635c5cb02121ac4ec34033a2
root <root@bt.foo.org>
2014-01-28 18:26:17 +0100

    routes.py

--- routes.py
+++ routes.py
@@ -1,6 +1,5 @@
-import os
-from flask import *
-#Flaisk, render_template
+from flask import Flask, render_template
+#import os
 
 app = Flask(__name__)
 @app.route('/')
@@ -11,5 +10,6 @@ def welcome():
         return render_template('welcome.html')
 if __name__ == '__main__':
 	#Bind to PORT if defined, otherwise default to 5000
-        port = int(os.environ.get('PORT', 5000))
-        app.run(host='0.0.0.0', port=port)
+#        port = int(os.environ.get('PORT', 5000))
+#        app.run(host='0.0.0.0', port=port)
+        app.run()
