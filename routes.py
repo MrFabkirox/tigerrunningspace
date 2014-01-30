f21aaa205274fc66f31dc09a061468dd650948f3
root <root@bt.foo.org>
2014-01-30 17:41:58 +0100

    add page2

--- routes.py
+++ routes.py
@@ -3,11 +3,17 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 @app.route('/')
+
+@app.route('/home')
 def home():
         return render_template('home.html')
-@app.route('/welcome')
-def welcome():
-        return render_template('welcome.html')
+@app.route('/page1')
+def page1():
+        return render_template('page1.html')
+@app.route('/page2')
+def page2():
+        return render_template('page2.html')
+    
 if __name__ == '__main__':
 	#Bind to PORT if defined, otherwise default to 5000
         port = int(os.environ.get('PORT', 5000))
