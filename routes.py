8deee0dac43ccd7d3bf8ea160616dfd49e19fee9
root <root@bt.foo.org>
2014-01-30 19:42:26 +0100

    log start

--- routes.py
+++ routes.py
@@ -14,7 +14,21 @@ def page1():
 def page2():
         return render_template('page2.html')
     
+@app.route('/hello')
+def hello():
+        return render_template('hello.html')
+    
 if __name__ == '__main__':
 	#Bind to PORT if defined, otherwise default to 5000
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
+        
+@app.route('/log', methods=['GET', 'POST'])
+def log()::
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            return redirect(url_for'hello'))
+    return render_template('log.html', error=error)
