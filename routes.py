73b51d6eca2bd88d9d207698ff8056ef27e0bd08
root <root@bt.foo.org>
2014-02-05 21:09:36 +0100

    log functional2

--- routes.py
+++ routes.py
@@ -1,7 +1,11 @@
 import os
 from flask import *
+from functools import wraps
 
 app = Flask(__name__)
+
+app.secret_key = 'my precious'
+
 @app.route('/')
 
 @app.route('/home')
@@ -16,10 +20,14 @@ def page1():
 def page2():
         return render_template('page2.html')
     
+# def login_required(test):
+    
+
 @app.route('/logout')
 def logout():
     session.pop('logged_in', None)
-    return redirect (url_for('home'))
+    flash('You were logged out')
+    return redirect (url_for('log'))
     
 @app.route('/hello')
 def hello():
@@ -32,12 +40,13 @@ def log():
         if request.form['username'] != 'admin' or request.form['password'] != 'admin':
             error = 'Invalid Credentials. Please try again.'
         else:
-#             session['logged_in'] = True
+            session['logged_in'] = True
             return redirect(url_for('hello'))
     return render_template('log.html', error=error)
     
 if __name__ == '__main__':
 #Bind to PORT if defined, otherwise default to 5000
+        app.run(debug=True)
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
         
