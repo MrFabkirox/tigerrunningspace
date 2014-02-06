d93c3011c67b880acc5a251622c3fa1e45daddc4
root <root@bt.foo.org>
2014-02-06 14:02:42 +0100

    log functional4

--- routes.py
+++ routes.py
@@ -4,7 +4,7 @@ from functools import wraps
 
 app = Flask(__name__)
 
-app.secret_key = 'my precious'
+# app.secret_key = 'my precious'
 
 @app.route('/')
 
@@ -46,7 +46,7 @@ def log():
     
 if __name__ == '__main__':
 #Bind to PORT if defined, otherwise default to 5000
-        app.run(debug=False)
+#         app.run(debug=False)
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
         
