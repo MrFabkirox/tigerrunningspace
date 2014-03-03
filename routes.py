59f671ebc7ec9a6e915545bc59b9b749b3590728
root <root@bt.foo.org>
2014-03-03 18:58:55 +0100

    menu css3

--- routes.py
+++ routes.py
@@ -3,9 +3,6 @@ from flask import *
 from functools import wraps
 import sqlite3
 
-# DATABASE = 'sales.db'
-# DATABASE = 'quotes.db'
-
 DATABASE = 'tiger.db'
 
 app = Flask(__name__)
@@ -15,6 +12,33 @@ app.secret_key = 'my precious'
 
 def connect_db():
     return sqlite3.connect(app.config['DATABASE'])
+
+def login_required(test):
+        @wraps(test)
+        def wrap(*args, **kwargs):
+            if 'logged_in' in session:
+                return test(*args, **kwargs)
+            else:
+                flash('You need to login first.')
+                return redirect(url_for('log'))
+        return wrap
+
+@app.route('/logout')
+def logout():
+    session.pop('logged_in', None)
+    flash('You were logged out')
+    return redirect (url_for('log'))
+
+@app.route('/page4', methods=['GET', 'POST'])
+def log():
+    error = None
+    if request.method == 'POST':
+        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
+            error = 'Invalid Credentials. Please try again.'
+        else:
+            session['logged_in'] = True
+            return redirect(url_for('page4'))
+    return render_template('log.html', error=error)
 
 @app.route('/')
 def home():
@@ -42,6 +66,11 @@ def page3():
         delete_quote = [dict (quote_id=row[0], quote=row[1]) for row in cur.fetchall()]
         g.db.close()
         return render_template('page3.html', new_quote=new_quote, delete_quote=delete_quote)
+    
+@app.route('/page4')
+@login_required
+def page4():
+        return render_template('page4.html')
 
 @app.route('/new_task', methods=['POST'])
 def new_task():
@@ -70,41 +99,14 @@ def complete(quote_id):
         flash('The post was validated')
         return redirect(url_for('page3'))
     
-def login_required(test):
-        @wraps(test)
-        def wrap(*args, **kwargs):
-            if 'logged_in' in session:
-                return test(*args, **kwargs)
-            else:
-                flash('You need to login first.')
-                return redirect(url_for('log'))
-        return wrap
-
-@app.route('/logout')
-def logout():
-    session.pop('logged_in', None)
-    flash('You were logged out')
-    return redirect (url_for('log'))
-    
 @login_required
 def hello():
        
     return render_template('hello.html')
     
-@app.route('/log', methods=['GET', 'POST'])
-def log():
-    error = None
-    if request.method == 'POST':
-        if request.form['username'] != 'admin' or request.form['password'] != 'admin':
-            error = 'Invalid Credentials. Please try again.'
-        else:
-            session['logged_in'] = True
-            return redirect(url_for('hello'))
-    return render_template('log.html', error=error)
-    
 if __name__ == '__main__':
 #Bind to PORT if defined, otherwise default to 5000
-#         app.run(debug=True)
+        app.run(debug=True)
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
         
