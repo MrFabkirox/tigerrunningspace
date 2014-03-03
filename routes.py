fb67fb6f301f1776166aad42e275996d32df729c
root <root@bt.foo.org>
2014-03-03 19:02:50 +0100

    menu css3 2

--- routes.py
+++ routes.py
@@ -106,7 +106,7 @@ def hello():
     
 if __name__ == '__main__':
 #Bind to PORT if defined, otherwise default to 5000
-        app.run(debug=True)
+#         app.run(debug=True)
         port = int(os.environ.get('PORT', 5000))
         app.run(host='0.0.0.0', port=port)
         
